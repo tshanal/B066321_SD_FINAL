0b9780fe066f43b6c66526f77f7abc2377418351
tshanal <tshanal@gmail.com>
2015-04-12 11:25:27 +0100

    Commit change

--- input.py
+++ input.py
@@ -3,103 +3,64 @@ import sys
 import traceback
 from biggroup import BigGroup
 from biggroup import GraphBit
+import json
 
 def main(argv):
-    nm = 10
+    nodes_count = 10
 
     if len(argv) >= 3 and argv[0]:
-        nm = int(argv[0])
-
-    if nm <= 10:
-        na = 20
-        ni = 12
-        nr = 1
+        nodes_count = int(argv[0])
     else:
-        na = 28
-        ni = 20
-        nr = 1
-
+	print "Expected 3 Inputs : <number of cities> <Name and Distance Picked file> <Output pickled file>" 		
+	sys.exit(0)
+	
+    if nodes_count <= 10:
+        ants_count = 20
+        iteration_count = 12
+        Outer_Repetition = 1
+    else:
+        ants_count = 28
+        iteration_count = 20
+        Outer_Repetition = 1
+    
+    			
     stuff = pickle.load(open(argv[1], "r"))
-    cities = stuff[0]
-    cm = stuff[1]
+    citiesArray = stuff[0]
+    distanceMatrix = stuff[1]
     #why are we doing this?
-    if nm < len(cm):
-        cm = cm[0:nm]
-        for i in range(0, nm):
-            cm[i] = cm[i][0:nm]
+    if nodes_count < len(distanceMatrix):
+        distanceMatrix = distanceMatrix[0:nodes_count]
+        for i in range(0, nodes_count):
+            distanceMatrix[i] = distanceMatrix[i][0:nodes_count]
 
 
 
     try:
-        graph = GraphBit(nm, cm)
-        bpv = None
-        bpc = sys.maxint
-        for i in range(0, nr):
+        graph = GraphBit(nodes_count, distanceMatrix)
+        optimalPath = None
+        optimalCostArray = sys.maxint
+        for i in range(0, Outer_Repetition):
             print "Repetition %s" % i
             graph.reset_tau()
-            workers = BigGroup(graph, na, ni)
+            workers = BigGroup(graph, ants_count, iteration_count)
             print "Colony Started"
             workers.start()
-            if workers.bpc < bpc:
+            if workers.optimalCostArray < optimalCostArray:
                 print "Colony Path"
-                bpv = workers.bpv
-                bpc = workers.bpc
+                optimalPath = workers.optimalPath
+                optimalCostArray = workers.optimalCostArray
 
         print "\n------------------------------------------------------------"
         print "                     Results                                "
         print "------------------------------------------------------------"
-        print "\nBest path = %s" % (bpv,)
+        print "\nBest path = %s" % (optimalPath,)
         city_vec = []
-        for node in bpv:
-            print cities[node] + " ",
-            city_vec.append(cities[node])
-        print "\nBest path cost = %s\n" % (bpc,)
-        results = [bpv, city_vec, bpc]
-        pickle.dump(results, open(argv[2], 'w+'))
-    except Exception, e:
-        print "exception: " + str(e)
-        traceback.print_exc()
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
-    stuff = pickle.load(open(argv[1], "r"))
-    cities = stuff[0]
-    cm = stuff[1]
-    #why are we doing this?
-    if nm < len(cm):
-        cm = cm[0:nm]
-        for i in range(0, nm):
-            cm[i] = cm[i][0:nm]
-
-
-
-    try:
-        graph = GraphBit(nm, cm)
-        bpv = None
-        bpc = sys.maxint
-        for i in range(0, nr):
-            print "Repetition %s" % i
-            graph.reset_tau()
-            workers = BigGroup(graph, na, ni)
-            print "Colony Started"
-            workers.start()
-            if workers.bpc < bpc:
-                print "Colony Path"
-                bpv = workers.bpv
-                bpc = workers.bpc
-
-        print "\n------------------------------------------------------------"
-        print "                     Results                                "
-        print "------------------------------------------------------------"
-        print "\nBest path = %s" % (bpv,)
-        city_vec = []
-        for node in bpv:
-            print cities[node] + " ",
-            city_vec.append(cities[node])
-        print "\nBest path cost = %s\n" % (bpc,)
-        results = [bpv, city_vec, bpc]
-        pickle.dump(results, open(argv[2], 'w+'))
+        for node in optimalPath:
+            print citiesArray[node] + " ",
+            city_vec.append(citiesArray[node])
+        print "\nBest path cost = %s\n" % (optimalCostArray,)
+        results = [optimalPath, city_vec, optimalCostArray]
+        json.dump(results, open(argv[2], 'w+') , skipkeys=True,  ensure_ascii=False)
     except Exception, e:
         print "exception: " + str(e)
         traceback.print_exc()
